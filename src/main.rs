5454622f01b58a2bbf7758f9b65745f2ad69782b
Hyounggyu Kim <code@hyounggyu.com>
2023-12-22 13:53:52 +0700

    Add LU decomposition fn.

--- src/main.rs
+++ src/main.rs
@@ -22,9 +22,59 @@ impl Matrix {
     }
 }
 
+fn lu_dcp(mat: &Matrix) -> (Matrix, Matrix) {
+    // TODO: Raise error if mat is not square
+    let mut l = Matrix::new(mat.rows, mat.cols);
+    let mut u = Matrix::new(mat.rows, mat.cols);
+
+    for i in 0..mat.rows {
+        l.set(i, i, 1.0);
+    }
+
+    for i in 0..mat.rows {
+        for j in 0..mat.cols {
+            if i <= j {
+                u.set(i, j, mat.get(i, j));
+                for k in 0..i {
+                    u.set(i, j, u.get(i, j) - l.get(i, k) * u.get(k, j));
+                }
+            } else {
+                l.set(i, j, mat.get(i, j));
+                for k in 0..j {
+                    l.set(i, j, l.get(i, j) - l.get(i, k) * u.get(k, j));
+                }
+                l.set(i, j, l.get(i, j) / u.get(j, j));
+            }
+        }
+    }
+
+    (l, u)
+}
+
+fn print_mat(mat: &Matrix) {
+    for i in 0..mat.rows {
+        for j in 0..mat.cols {
+            print!("{} ", mat.get(i, j));
+        }
+        println!();
+    }
+}
+
 fn main() {
-    let mut a: Matrix = Matrix::new(2, 3);
-    println!("a: {:?}", a.data);
-    a.set(0, 0, 1.0);
-    println!("a: {:?}", a.data);
+    let mut a: Matrix = Matrix::new(3, 3);
+    let data = vec![2, -1, 0, -1, 2, -1, 0, -1, 2];
+    println!("Matrix a:");
+    print_mat(&a);
+    for i in 0..a.rows {
+        for j in 0..a.cols {
+            a.set(i, j, data[i * a.cols + j] as f64);
+        }
+    }
+    println!("Matrix a:");
+    print_mat(&a);
+    let (l, u) = lu_dcp(&a);
+    println!("Matrix l:");
+    print_mat(&l);
+    println!("Matrix u:");
+    print_mat(&u);
 }
