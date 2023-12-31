49369be8dc550341babd25f32b88efd7783db19d
Hyounggyu Kim <code@hyounggyu.com>
2023-12-31 14:24:20 +0700

    Update lu_c

--- lu_c/main.c
+++ lu_c/main.c
@@ -3,20 +3,15 @@
 
 #include "linalg_lu.h"
 
-const double data[9] = {0, 2, 3, 4, 5, 6, 7, 8, 9};
+const double a_data[9] = {3, 2, 0, 1, -1, 0, 0, 5, 1};
 const int kN = 3;
 const double kTol = 1e-6;
 
 void print_perm(int *p, int n) {
-  printf("p = [ ");
-  for (int i = 0; i < n + 1; i++) {
-    printf("%d ", p[i]);
-  }
-  printf("]\n\n");
   for (int i = 0; i < n; i++) {
     printf("[ ");
     for (int j = 0; j < n; j++) {
-      printf("%d ", i == p[j]);
+      printf("%+3d ", i == p[j]);
     }
     printf("]\n");
   }
@@ -26,10 +21,26 @@ void print_mat(double **a, int n) {
   for (int i = 0; i < n; i++) {
     printf("[ ");
     for (int j = 0; j < n; j++) {
-      printf("%f ", a[i][j]);
+      printf("%+3.5f ", a[i][j]);
     }
     printf("]\n");
   }
+}
+
+void print_ivec(int *v, int n) {
+  printf("[ ");
+  for (int i = 0; i < n; i++) {
+    printf("%+3d ", v[i]);
+  }
+  printf("]\n");
+}
+
+void print_dvec(double *v, int n) {
+  printf("[ ");
+  for (int i = 0; i < n; i++) {
+    printf("%+3.5f ", v[i]);
+  }
+  printf("]\n");
 }
 
 int main() {
@@ -44,20 +55,39 @@ int main() {
 
   for (int i = 0; i < kN; i++) {
     for (int j = 0; j < kN; j++) {
-      a[i][j] = data[i * kN + j];
+      a[i][j] = a_data[i * kN + j];
     }
   }
 
-  printf("Initial matrix:\n");
+  printf("Matrix A is\n");
   print_mat(a, kN);
   printf("\n");
 
   LUPDecompose(a, kN, kTol, p);
 
-  printf("LUPDecompose:\n");
+  printf("L - I + U is\n");
   print_mat(a, kN);
   printf("\n");
 
-  printf("Permutation matrix:\n");
+  printf("Permutation data:\n");
+  print_ivec(p, kN);
+  printf("\n");
+
+  printf("P is\n");
   print_perm(p, kN);
+  printf("\n");
+
+  double x[kN];
+  double b[kN] = {2, 4, -1};
+
+  printf("Solve Ax = b:\n\n");
+  printf("b is,\n");
+  print_dvec(b, kN);
+  printf("\n");
+
+  LUPSolve(a, p, b, kN, x);
+
+  printf("x is,\n");
+  print_dvec(x, kN);
+  printf("\n");
 }
